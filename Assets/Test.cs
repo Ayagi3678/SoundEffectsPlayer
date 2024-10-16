3db8601bbc9607ff5f9bcbf98765d0097306f203
Ayagi3678 <114006559+Ayagi3678@users.noreply.github.com>
2024-10-16 20:16:39 +0900

    ErrorLog

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -14,6 +14,10 @@ namespace DefaultNamespace
         {
             SoundFXPlayer.SetAudioMixerGroup(soundFXAudioMixerGroup);
         }
+        private void TestMethod(ResourcesAudioFile audioFile)
+        {
+            SoundFXPlayer.Play(audioFile);
+        }
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
